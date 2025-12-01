0d77f520aad27f6de098b5874f046d4cb36332b6
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-12-01 16:58:23 +0300

    Create our namespace

--- main.cpp
+++ main.cpp
@@ -1,15 +1,26 @@
 
-struct p_t {
-  int x,y;
-};
-
-struct IDraw {
-
-  virtual p_t begin()=0;
-  virtual p_t next() = 0;
-};
 
 
+namespace top {
+  struct p_t {
+    int x,y;
+  };
+
+  struct IDraw {
+    virtual p_t begin()=0;
+    virtual p_t next() = 0;
+  };
+
+  bool operator==(p_t a, p_t b)
+  {
+    return a.x == b.x && a.y == b.y;
+  }
+
+  bool operator!=(p_t a, p_t b)
+  {
+    return !(a == b);
+  }
+}
 
 
 
