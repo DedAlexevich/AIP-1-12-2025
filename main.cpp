dbfb007e751b9247425b511040a85e20f7533bd1
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-12-08 17:46:32 +0300

    ONO RABOTAET URAAAAAA

--- main.cpp
+++ main.cpp
@@ -38,7 +38,7 @@ namespace top {
   size_t getPoints(IDraw* f, p_t** ps, size_t& s);
   Frame_t buildFrame(const p_t* ps, size_t s); // Ищем мин и макс для х и у
   char* buildCanvas(Frame_t fr, char); // на основе фрейма считаем макс - мин + 1
-  void paintCanvas(char* cnv, Frame_t fr, p_t p, size_t k, char f); // координаты перевести в коорд канваса ужас
+  void paintCanvas(char* cnv, Frame_t fr, p_t * p, size_t k, char f); // координаты перевести в коорд канваса ужас
   void printCanvas(char* cnv, Frame_t fr);  // ТОЛЬКО ПОПРОБУЙ ВЫВЕСТИ ЛИШНИЙ ПРОБЕЛ!!!!
 
   struct VLine : IDraw {
@@ -71,19 +71,19 @@ namespace top {
 
 int main()
 {
-  top::IDraw* f[3] = {};
-  top::p_t* p = nullptr;
-  size_t s = 3;
+  top::IDraw* f[5] = {};
+  top::p_t* p = new top::p_t[1];
+  size_t s = 0;
   char* cnv = nullptr;
   int statusCode = 0;
   try {
-    top::make_f(f, 3);
-    for (size_t i = 0; i < 3; ++i) {
+    top::make_f(f, 5);
+    for (size_t i = 0; i < 5; ++i) {
       top::getPoints(f[i], &p, s);
     }
     top::Frame_t fr = top::buildFrame(p, s);
-    cnv = top::buildCanvas(fr, '0');
-    top::paintCanvas(cnv, fr, *p, s, 'o');
+    cnv = top::buildCanvas(fr, '.');
+    top::paintCanvas(cnv, fr, p, s, 'o');
     top::printCanvas(cnv, fr);
   } catch(...) {
     statusCode = 1;
@@ -107,13 +107,15 @@ void top::make_f(IDraw** f, size_t k)
 {
   f[0] = new Dot(0, 0);
   f[1] = new Dot(1, -5);
-  f[2] = new Dot(7, 7);
+  f[2] = new Square(7, 7, 3);
+  f[3] = new VLine(4, 5, 3);
+  f[4] = new HLine(2, 7, 4);
 }
 
 void top::extend(p_t** ps, size_t s, p_t p)
 {
   size_t upd_s = s + 1;
-  top::p_t* res = new top::p_t[upd_s];
+  p_t* res = new p_t[upd_s];
   for (size_t i = 0; i < s; ++i) {
     res[i] = (*ps)[i];
   }
@@ -132,7 +134,7 @@ size_t top::getPoints(IDraw* f, p_t** ps, size_t& s)
     extend(ps, s+delta, p);
     delta++;
   }
-   return delta;
+   return s+=delta;
 }
 
 top::Frame_t top::buildFrame(const p_t* ps, size_t s)
@@ -174,11 +176,13 @@ char* top::buildCanvas(Frame_t fr, char fill)
   return newcanws;
 }
 
-void top::paintCanvas(char* cnv, Frame_t fr, p_t p, size_t k, char f)
+void top::paintCanvas(char* cnv, Frame_t fr, p_t * p, size_t k, char f)
 {
-  int dx = p.x - fr.leftBott.x;
-  int dy = fr.rightTop.y - p.y;
-  cnv[dy * cols(fr) + dx] = f;
+  for (size_t i = 0; i < k; ++i) {
+    int dx = p[i].x - fr.leftBott.x;
+    int dy = fr.rightTop.y - p[i].y;
+    cnv[dy * cols(fr) + dx] = f;
+  }
 }
 
 void top::printCanvas(char* cnv, Frame_t fr)
@@ -252,10 +256,9 @@ top::Square::Square(int x, int y, int l):
   }
 }
 
-top::Square::Square(p_t p, int l)
-{
-  Square(p.x, p.y, l);
-}
+top::Square::Square(p_t p, int l):
+  Square(p.x, p.y, l)
+{}
 
 top::p_t top::Square::begin() const
 {
@@ -273,8 +276,12 @@ top::p_t top::Square::next(p_t p) const
   } else if (p.y == start.y && p.x > start.x) {
     return {p.x - 1, p.y};
   }
+  return start;
 }
 
-
+// HLine VLine  clw
+// 45dgre Line clw
+// Square  hmw
+// Rectangl   hmw
 
 
