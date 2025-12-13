59ce58fa2c3f38452bbe8d66580784905bbe8414
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-12-13 12:01:06 +0300

    edit Square

--- main.cpp
+++ main.cpp
@@ -65,8 +65,13 @@ namespace top {
     int a_, b_;
   };
 
-  struct Square: Rectangle {
+  struct Square: IDraw {
     Square(int x, int y, int l);
+    Square(p_t p, int l);
+    p_t begin() const override;
+    p_t next(p_t p) const override;
+    p_t start;
+    int len;
   };
 
   struct Triangl: IDraw {
@@ -267,8 +272,37 @@ top::p_t top::HLine::next(p_t p) const
 }
 
 top::Square::Square(int x, int y, int l):
-  Rectangle(x, y, l, l)
-{}
+  IDraw(),
+  start{x, y},
+  len(l)
+{
+  if (len <= 0) {
+    throw std::invalid_argument("lenght can not be  <= 0");
+  }
+}
+
+top::Square::Square(p_t p, int l)
+{
+  Square(p.x, p.y, l);
+}
+
+top::p_t top::Square::begin() const
+{
+  return start;
+}
+
+top::p_t top::Square::next(p_t p) const
+{
+  if (p.x == start.x && p.y < start.y + len - 1) {
+    return {p.x, p.y + 1};
+  } else if (p.y == start.y + len - 1 && p.x < start.x + len - 1) {
+    return {p.x + 1, p.y};
+  } else if (p.x == start.x + len - 1 && p.y > start.y) {
+    return {p.x, p.y - 1};
+  } else if (p.y == start.y && p.x > start.x) {
+    return {p.x - 1, p.y};
+  }
+}
 
 top::DLine::DLine(int x, int y, int len):
   start{x, y},
