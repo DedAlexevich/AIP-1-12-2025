ed04a8550cf8dbc9f3052e680b17162666903deb
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-12-25 15:03:44 +0300

    edit cli, array of fun

--- cli.cpp
+++ cli.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+void hi()
+{
+  std::cout << "<Hi>\n";
+}
 int main()
 {
+  size_t i = 0;
+  void(*cmds[1])() = {hi};
+  constexpr size_t COUNT_COMMAND = 1;
 
+  while (!(std::cin >> i).eof()) {
+    if (std::cin.fail()) {
+      std::cerr << "INVALID INPUT\n";
+      //Propusk vvoda
+
+      return 1;
+    } else if (i < COUNT_COMMAND) {
+      cmds[i]();
+    } else {
+      std::cerr << "INKNOWN COM\n";
+    }
+
+  }
 }
